68030c86089a048aa3a56678946efb45b4db3069
Zo Sarillana <zhou991818@gmail.com>
2024-06-24 14:34:32 +0800

    timeline update

--- src/components/sections/hero/Timeline.jsx
+++ src/components/sections/hero/Timeline.jsx
@@ -31,38 +31,6 @@ export const Timeline = () => {
                   />
                 </svg>
               </div>
-              <div className="timeline-end mb-10">
-                <time className="font-mono italic">2010 - 2014</time>
-                <div className="text-lg font-black">
-                  Camillo Osias Elementary School - Introduction to Computers
-                </div>
-                I was introduced to video games and multiplayer experiences
-                began with my PSP and GameBoy, where I embarked on thrilling
-                adventures. From hunting monsters and crafting items to defeat
-                the Ancient Dragon in Monster Hunter, to stomping on Goombas and
-                defeating Bowser to save the princess in tunnels upon tunnels,
-                each game brought its own unique challenges and excitement.
-                Additionally, I found enjoyment in strategizing in DOTA to
-                defend the Ancients, and exploring a wide variety of other
-                engaging video games that fueled my passion for immersive gaming
-                experiences.
-              </div>
-              <hr />
-            </li>
-            <li>
-              <div className="timeline-middle">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="h-5 w-5">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
               <div className="timeline-start md:text-end mb-10">
                 <time className="font-mono italic">2014 - 2018</time>
                 <div className="text-lg font-black">
